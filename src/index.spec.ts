e3f37436de726740c003f994102719018245bc78
jatin510 <jatin6972@gmail.com>
2022-10-02 20:42:49 +0530

    password should be of the required length

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,18 @@
-describe('test', () => {
-  test('add', async () => {
-    expect(1 + 1).toEqual(2);
+import { IPasswordProps, Password } from './index';
+
+describe('Password generator', () => {
+  test('should be able to generate a password', () => {
+    const passwordInput: IPasswordProps = { password: 'jatin' };
+    const newPassword = Password.create(passwordInput);
+    expect(newPassword.password).toEqual(passwordInput.password);
+  });
+
+  test('password should have length between 5-15 characters', () => {
+    const passwordInput: IPasswordProps = { password: 'ja' };
+
+    const passwordOutput = () => {
+      Password.create(passwordInput);
+    };
+    expect(passwordOutput).toThrow(RangeError);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-// eslint-disable-next-line no-console
-console.log('Hello world!');
+export interface IPasswordProps {
+  password: string;
+}
+export class Password {
+  password: string;
+  private constructor(props: IPasswordProps) {
+    this.password = props.password;
+  }
+
+  public static validateLength(password: string): boolean {
+    const passwordLength = password.length;
+    if (passwordLength < 5 || passwordLength > 15) {
+      return false;
+    }
+    return true;
+  }
+
+  public static create(props: IPasswordProps) {
+    const { password } = props;
+
+    const isValidLength = this.validateLength(password);
+    if (!isValidLength) {
+      throw new RangeError(`Password length should be between 5-15 length`);
+    }
+
+    const newPassword = new Password(props);
+    return newPassword;
+  }
+}
