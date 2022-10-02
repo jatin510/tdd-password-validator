cf51559ae82d2ffd99efcc6754e4460f38e9ccab
jatin510 <jatin6972@gmail.com>
2022-10-02 21:04:48 +0530

    password should atleast 1 digit and 1 Uppercase character

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,17 +2,39 @@ import { IPasswordProps, Password } from './index';
 
 describe('Password generator', () => {
   test('should be able to generate a password', () => {
-    const passwordInput: IPasswordProps = { password: 'jatin' };
+    const passwordInput: IPasswordProps = { password: 'Jatin123' };
     const newPassword = Password.create(passwordInput);
     expect(newPassword.password).toEqual(passwordInput.password);
   });
 
   test('password should have length between 5-15 characters', () => {
-    const passwordInput: IPasswordProps = { password: 'ja' };
+    const passwordInput: IPasswordProps = { password: 'Ja1' };
 
     const passwordOutput = () => {
       Password.create(passwordInput);
     };
     expect(passwordOutput).toThrow(RangeError);
+  });
+
+  test('password should contain at least 1 digit', () => {
+    const passwordInput: IPasswordProps = { password: 'Jatin' };
+
+    const passwordOutput = () => {
+      Password.create(passwordInput);
+    };
+    expect(passwordOutput).toThrowError(
+      'Password should contain at least 1 digit',
+    );
+  });
+
+  test('password should contain at least 1 digit', () => {
+    const passwordInput: IPasswordProps = { password: 'jatin1' };
+
+    const passwordOutput = () => {
+      Password.create(passwordInput);
+    };
+    expect(passwordOutput).toThrowError(
+      'Password should contain at least 1 Uppercase letter',
+    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,34 @@ export class Password {
     return true;
   }
 
+  public static validatePasswordContainsDigit(password: string): boolean {
+    const pattern = new RegExp(/[0-9]/);
+    return !!password.match(pattern);
+  }
+
+  public static validatePasswordContainsUppercaseLetter(password: string) {
+    const pattern = new RegExp(/[A-Z]/);
+    return !!password.match(pattern);
+  }
+
   public static create(props: IPasswordProps) {
     const { password } = props;
 
     const isValidLength = this.validateLength(password);
     if (!isValidLength) {
       throw new RangeError(`Password length should be between 5-15 length`);
+    }
+
+    const isPasswordContainsDigit =
+      this.validatePasswordContainsDigit(password);
+    if (!isPasswordContainsDigit) {
+      throw new Error('Password should contain at least 1 digit');
+    }
+
+    const isPasswordContainsUppercaseLetter =
+      this.validatePasswordContainsUppercaseLetter(password);
+    if (!isPasswordContainsUppercaseLetter) {
+      throw new Error('Password should contain at least 1 Uppercase letter');
     }
 
     const newPassword = new Password(props);
